5fcf67f812c841aeaa03213a23a12845c95b6c22
DAB0mB <emanor6@gmail.com>
2019-03-10 15:02:12 +0800

    Step 2.3: Setup CSS theme vars

--- src/components/ChatsListScreen/ChatsNavbar.tsx
+++ src/components/ChatsListScreen/ChatsNavbar.tsx
@@ -1,9 +1,18 @@
+import { Toolbar } from '@material-ui/core';
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled(Toolbar) `
+  background-color: var(--primary-bg);
+  color: var(--primary-text);
+  font-size: 20px;
+  line-height: 40px;
+` as typeof Toolbar;
 
 const ChatsNavbar = () => (
-  <div>
+  <Container>
     Whatsapp Clone
-  </div>
+  </Container>
 );
 
 export default ChatsNavbar;
